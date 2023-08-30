a07048a354b618bbc189b7c6338fa2d7540fdf11
Ana Beatriz Braz <anabeatrizbraz@Anas-MacBook-Pro.local>
2023-08-30 20:29:15 -0300

    adding tree plot

--- lista2/ex2.py
+++ lista2/ex2.py
@@ -10,18 +10,16 @@ from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from yellowbrick.classifier import ConfusionMatrix
+from sklearn import tree
 
-base = pd.read_csv('restaurante.csv', usecols=['Instancia','Bar','Sex/Sab','Fome','Cliente','Preco','Chuva','Res','Tipo','Tempo','Conclusao']) # carregando a base csv
+# Carregando o csv
+data = pd.read_csv('restaurante.csv', usecols=['Instancia','Bar','Sex/Sab','Fome','Cliente','Preco','Chuva','Res','Tipo','Tempo','Conclusao'])
 
-# sns.countplot(x = base['Conclusao'])
-
-X_prev = base.iloc[:, 0:10].values
-X_prev_label = base.iloc[:, 0:10]
-y_classe = base.iloc[:, 10].values
-
+# Declarando os valores de cada posição + o rótulo
+X_prev = data.iloc[:, 0:10].values
+label = data.iloc[:, 10].values
 
 # Tratando dados categóricos
-
 label_encoder = LabelEncoder()
 
 label_encoder_Alternativo = LabelEncoder()
@@ -45,20 +43,29 @@ X_prev[:,9] = label_encoder_SexSab.fit_transform(X_prev[:,9])
 
 # One Hot Enconder -> binarizar atributos não ordinais
 
-onehotencoder_restaurante = ColumnTransformer(transformers=[('OneHot', OneHotEncoder(), [8])], remainder='passthrough')
+onehotencoder_data = ColumnTransformer(transformers=[('OneHot', OneHotEncoder(), [8])], remainder='passthrough')
 
-X_prev= onehotencoder_restaurante.fit_transform(X_prev)
+X_prev= onehotencoder_data.fit_transform(X_prev)
 
 # print(X_prev)
 
 # Holdout
-X_treino, X_teste, y_treino, y_teste = train_test_split(X_prev, y_classe, test_size = 0.20, random_state = 23)
+X_treino, X_teste, y_treino, y_teste = train_test_split(X_prev, label, test_size = 0.20, random_state = 23)
 
 # Decision tree
+model = DecisionTreeClassifier(criterion='entropy')
+Y = model.fit(X_treino, y_treino)
+prevision = model.predict(X_teste)
+# print(y_teste)
+# print(accuracy_score(y_teste,prevision))
+confusion_matrix(y_teste, prevision)
 
-modelo = DecisionTreeClassifier(criterion='entropy')
-Y = modelo.fit(X_treino, y_treino)
-previsoes = modelo.predict(X_teste)
-print(y_teste)
-print(accuracy_score(y_teste,previsoes))
-confusion_matrix(y_teste, previsoes)
+cm = ConfusionMatrix(model)
+cm.fit(X_treino, y_treino)
+cm.score(X_teste, y_teste)
+
+# print(classification_report(y_teste, prevision))
+
+# Plotando a árvore
+tree.plot_tree(Y)
+plt.show()
