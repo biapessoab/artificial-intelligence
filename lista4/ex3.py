e3947c47c182431931a768054f57dd00651ba5fe
Ana Beatriz Braz <anabeatrizbraz@Anas-MacBook-Pro.local>
2023-09-14 15:43:14 -0300

    adding ex3 lista4

--- lista4/ex3.py
+++ lista4/ex3.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 data = pd.read_csv('./data.csv')
 
@@ -17,8 +19,14 @@ X[:, 1] = label_encoder_temperatura.fit_transform(X[:, 1])
 X[:, 2] = label_encoder_umidade.fit_transform(X[:, 2])
 X[:, 3] = label_encoder_ventando.fit_transform(X[:, 3])
 
-naive = GaussianNB()
-naive = naive.fit(X, Y)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
-previsao = naive.predict([[0,1,1,0]])
+random_forest = RandomForestClassifier(n_estimators=100, random_state=42)
+
+random_forest.fit(X_train, Y_train)
+
+Y_pred = random_forest.predict(X_test)
+
+# Fazer previsões nos dados de teste
+previsao = random_forest.predict([[0,1,1,0]])
 print(previsao)
